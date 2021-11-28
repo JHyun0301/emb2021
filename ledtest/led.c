6e063ac9588b81e40e750b803c118cf94044b997
sumin Yun <yunsm238@mju.ac.kr>
2021-11-28 14:36:47 +0900

    led.c
    status 함수 추가

--- ledtest/led.c
+++ ledtest/led.c
@@ -1,14 +1,28 @@
+#include <stdlib.h>
+#include <fcntl.h>
 #include "led.h"
+#include <unistd.h>
 
 static unsigned int ledValue = 0;
 static int fd = 0;
-int leddOnOff(int ledNum, int onOff)
+
+int ledOnOff(int ledNum, int onOff)
 {
 	int i=1;
 	i = i<<ledNum;
 	ledValue = ledValue& (~i);
 	if (onOff != 0) ledValue |= i;
 	write (fd, &ledValue, 4);
+}
+
+int ledStatus(void)
+{
+	printf("%d, 2진수 변환: 1 = 켜져있는 led, 0 = 꺼져있는 led", ledValue);
+	for(int j = 7; j>= 0; --j)
+	{
+		int result = ledValue >> j &1;
+			printf("%d", result);
+	}
 }
 
 int ledLibInit(void)
