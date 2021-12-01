239e1c6c480c9ffb6cb89b1d56f62d7a99562fd9
sumin Yun <yunsm238@mju.ac.kr>
2021-12-02 01:32:54 +0900

    buttontest.c

---
+++ ledtest/buttontest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+
+int main(void)
+{
+	buttonInit();
+}
+	
