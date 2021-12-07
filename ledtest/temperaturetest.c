24e8a28f8196cfcb247aed990c382a026e30315d
sumin Yun <yunsm238@mju.ac.kr>
2021-12-07 15:17:07 +0900

    temperaturetest.c

---
+++ ledtest/temperaturetest.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <getopt.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <linux/types.h>
+#include <linux/spi/spidev.h>
+#include "temperature.h"
+
+int main(int argc, char **argv)
+{
+	int printtemp();
+
+	return 0;
+}
+
