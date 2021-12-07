a492452938d0b46ff430ae9cf0045ddd37aecd73
sumin Yun <yunsm238@mju.ac.kr>
2021-12-07 15:16:09 +0900

    accelMagGyrotest.c

---
+++ ledtest/accelMagGyrotest.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include "accelMagGyro.h"
+
+int main(void){
+
+ Accelerometer();
+ Magnetometer(); 
+ Gyroscope();
+
+}
