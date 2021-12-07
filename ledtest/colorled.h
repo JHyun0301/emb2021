d277031d2d7536840fa1b52941ef33a2fa006ea3
sumin Yun <yunsm238@mju.ac.kr>
2021-12-07 18:09:51 +0900

    colorled.h

--- ledtest/colorled.h
+++ ledtest/colorled.h
@@ -1,11 +1,11 @@
 #ifndef _COLORLED_H_
 #define _COLORLED_H_
 
-void pwmActive(int bActivate);
+void pwmActive(int bActivate, int pwmIndex);
 void writePWMDuty(int  DutyCycle , int pwmIndex);
 void writePWMPeriod(int frequency, int pwmIndex) ;
-void pwmEnable(int bEnable);
-int pwmLedInit(void);
+void pwmEnable(int bEnable, int pwmIndex);
+
 
 #define MAX_SCALE_STEP		7
 
@@ -19,6 +19,27 @@ int pwmLedInit(void);
 #define PWM_EXPORT "export"
 #define PWM_UNEXPORT "unexport"
 
+#define PWM_RED_BASE_SYS_PATH_PWM_ENABLE_NAME "/sys/class/pwm/pwmchip0/pwm0/enable"	
+#define PWM_GREEN_BASE_SYS_PATH_PWM_ENABLE_NAME "/sys/class/pwm/pwmchip1/pwm0/enable"	
+#define PWM_BLUE_BASE_SYS_PATH_PWM_ENABLE_NAME "/sys/class/pwm/pwmchip2/pwm0/enable"	
+
+#define PWM_RED_BASE_SYS_PATH_PWM_EXPORT "/sys/class/pwm/pwmchip0/pwm0/export"	
+#define PWM_GREEN_BASE_SYS_PATH_PWM_EXPORT "/sys/class/pwm/pwmchip1/pwm0/export"	
+#define PWM_BLUE_BASE_SYS_PATH_PWM_EXPORT "/sys/class/pwm/pwmchip2/pwm0/export"	
+
+#define PWM_RED_BASE_SYS_PATH_PWM_UNEXPORT "/sys/class/pwm/pwmchip0/pwm0/unexport"	
+#define PWM_GREEN_BASE_SYS_PATH_PWM_UNEXPORT "/sys/class/pwm/pwmchip1/pwm0/unexport"	
+#define PWM_BLUE_BASE_SYS_PATH_PWM_UNEXPORT "/sys/class/pwm/pwmchip2/pwm0/unexport"	
+
+#define PWM_RED_BASE_SYS_PATH_PWM_FREQUENCY_NAME "/sys/class/pwm/pwmchip0/pwm0/period"	
+#define PWM_GREEN_BASE_SYS_PATH_PWM_FREQUENCY_NAME "/sys/class/pwm/pwmchip1/pwm0/period"	
+#define PWM_BLUE_BASE_SYS_PATH_PWM_FREQUENCY_NAME "/sys/class/pwm/pwmchip2/pwm0/period"
+
+
+#define PWM_RED_BASE_SYS_PATH_PWM_DUTYCYCLE_NAME "/sys/class/pwm/pwmchip0/pwm0/duty_cycle"
+#define PWM_GREEN_BASE_SYS_PATH_PWM_DUTYCYCLE_NAME "/sys/class/pwm/pwmchip1/pwm0/duty_cycle"	
+#define PWM_BLUE_BASE_SYS_PATH_PWM_DUTYCYCLE_NAME "/sys/class/pwm/pwmchip2/pwm0/duty_cycle"
+
 #define RED_INDEX	0
 #define GREEN_INDEX	1
 #define BLUE_INDEX	2
@@ -31,4 +52,3 @@ int pwmLedInit(void);
 #define  MAX_INPUT_VALUE	100
 
 #endif
-
