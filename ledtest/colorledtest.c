13588e8e3729c4bfa77dcb188c0b0564b479ec1f
sumin Yun <yunsm238@mju.ac.kr>
2021-12-07 18:10:00 +0900

    colorledtest.c

--- ledtest/colorledtest.c
+++ ledtest/colorledtest.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <linux/input.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <sys/ioctl.h>
+#include <ctype.h>
+#include <sys/ipc.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "colorled.h"
-
 
 int main(int argc, char *argv[])
 {
-	if (argc != 4)
-	{
-	       	printf ("colorledtest  0-100 0-100 0-100\r\n");
-		printf ("ex) colorledtest.elf 100 100 100 ==> full white color\r\n");
-		return 0; }
+	int red = atoi(argv[1]);
+	int green = atoi(argv[2]);
+	int blue = atoi(argv[3]);
 
-	pwmLedInit();
+	// inverse
+	red = MAX_INPUT_VALUE - red;
+	green = MAX_INPUT_VALUE - green;
+	blue = MAX_INPUT_VALUE - blue;
 	
+	// percentage
+	int redduty = PWM_FREQUENCY * red / MAX_INPUT_VALUE;
+	int greenduty = PWM_FREQUENCY * green / MAX_INPUT_VALUE;
+	int blueduty = PWM_FREQUENCY * blue / MAX_INPUT_VALUE;
+
+	pwmActive(1,RED_INDEX);
+    writePWMPeriod(PWM_FREQUENCY, RED_INDEX);
+	writePWMDuty(redduty, RED_INDEX);
+	pwmEnable(1,RED_INDEX);
+
+    pwmActive(1,GREEN_INDEX);
+	writePWMPeriod(PWM_FREQUENCY, GREEN_INDEX);
+	writePWMDuty(greenduty, GREEN_INDEX);
+	pwmEnable(1,GREEN_INDEX);
+ 
+    pwmActive(1,BLUE_INDEX);
+	writePWMPeriod(PWM_FREQUENCY, BLUE_INDEX);
+	writePWMDuty(blueduty, BLUE_INDEX);
+	pwmEnable(1,BLUE_INDEX);
+	
+    sleep(3);
+
+	return 0;
 }
 
