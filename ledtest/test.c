c4d8f4155755e4b57c03894451884ed068268b81
sumin357 <yunsm2385@mju.ac.kr>
2021-12-15 15:22:25 +0900

    test.c
    fnd 추가

--- ledtest/test.c
+++ ledtest/test.c
@@ -13,9 +13,11 @@
 #include <dirent.h>
 #include <sys/ioctl.h>
 #include <sys/msg.h>
+#include <time.h>
 
 #include "led.h"
 #include "buzzer.h"
+#include "fnd.h"
 
 #define LED_DRIVER_NAME "/dev/periled"
 
@@ -24,6 +26,8 @@ int main(int argc, char **argv)
 	unsigned int data = 0;
 	unsigned int onoff = 0;
     int scale;
+     int number;
+    int dot = 0;
 
 	if (argc < 2)
 	{
@@ -33,9 +37,16 @@ int main(int argc, char **argv)
 
 	data = strtol(argv[1], NULL, 16);
 	printf("wrate data : 0x%X\n", data);
+
 	onoff = strtol(argv[2], NULL, 16);
+
     scale = atoi(argv[1]);
     printf("scale : %d \n", scale);
+
+    number = atoi(argv[1]);
+     printf("number : %d\r\n", number);
+    printf("dot : %d\r\n", dot);
+
 
 
    ledLibInit();
@@ -43,6 +54,7 @@ int main(int argc, char **argv)
 	
    ledOnOff(data,onoff); 
    buzzerPlaySong(scale);
+    FNDWrite(number, 0);
 
    
    ledStatus();
@@ -50,7 +62,13 @@ int main(int argc, char **argv)
 
    sleep(1);
    buzzerStopSong();
+   fndOff();
 
+    count_discount(number);
+    fndOff();
+
+
+    FNDExit();
    buzzerExit();
    ledLibExit();
 
