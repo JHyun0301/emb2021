9b20a638cb5ab62734d3466e74a78e4da2442634
sumin357 <yunsm2385@mju.ac.kr>
2021-12-16 19:14:56 +0900

    button.h
    헤더파일

--- ledtest/button.h
+++ ledtest/button.h
@@ -1,4 +1,4 @@
-#ifndef _BUTTON_H_
+/*#ifndef _BUTTON_H_
 #define _BUTTON_H
 
 #define MESSAGE_ID 1122
@@ -14,5 +14,25 @@ int buttonInit(void);
 int buttonThread(void);
 void* buttonThFunc(void *arg);
 
+#endif*/
+
+#ifndef _BUTTON_H_
+#define _BUTTON_H_
+
+#define BUTTON_DRIVER_NAME "/dev/input/event4"
+#define MESSAGE_ID 1122
+typedef struct
+{
+	long int messageNum;
+	int keyInput;
+	int pressed;
+}BUTTON_MSG_T;
+
+int buttonLibInit(void);
+int buttonLibExit(void);
+//static void* buttonThFunc(void* arg);
+
 #endif
+
+#
 
